c199a0844f8e87e034ce5482a8f5120cbe156b7c
Luan Fonseca <34408591+tiluan@users.noreply.github.com>
2024-02-04 13:25:28 -0300

    Update: README.md dataset factory linkedService notebooks pipeline publish_config.json

--- notebooks/hnk-Lakehouse/2 - Prata/AdventureWorks/01 - Address.sql
+++ notebooks/hnk-Lakehouse/2 - Prata/AdventureWorks/01 - Address.sql
@@ -1,4 +1,4 @@
 -- Databricks notebook source
-CREATE OR REPLACE TABLE adventure_works_prata.address AS
+CREATE OR REPLACE TABLE adventure_works_prata.Address AS
 
-SELECT * FROM adventure_works_bronze.address;
+SELECT * FROM adventure_works_bronze.Address;
