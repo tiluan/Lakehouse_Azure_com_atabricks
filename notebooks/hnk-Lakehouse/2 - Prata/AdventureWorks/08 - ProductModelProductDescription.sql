d9276fbf11e62167dedc63dc7f8436d659f87478
Luan Fonseca <34408591+tiluan@users.noreply.github.com>
2024-02-04 13:28:20 -0300

    Update: README.md dataset factory linkedService notebooks pipeline publish_config.json

---
+++ notebooks/hnk-Lakehouse/2 - Prata/AdventureWorks/08 - ProductModelProductDescription.sql
@@ -0,0 +1,4 @@
+-- Databricks notebook source
+CREATE OR REPLACE TABLE adventure_works_prata.Product_Model_Product_Description AS
+
+SELECT * FROM adventure_works_bronze.Product_Model_Product_Description;
