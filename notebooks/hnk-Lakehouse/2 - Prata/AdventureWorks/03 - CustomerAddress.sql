59b8cc108b1380a713b64b796f9f8d8a7818f132
Luan Fonseca <34408591+tiluan@users.noreply.github.com>
2024-02-04 10:45:05 -0300

    Update: README.md dataset factory linkedService notebooks pipeline publish_config.json

---
+++ notebooks/hnk-Lakehouse/2 - Prata/AdventureWorks/03 - CustomerAddress.sql
@@ -0,0 +1,7 @@
+-- Databricks notebook source
+CREATE OR REPLACE TABLE adventure_works_prata.customer_addressr AS
+
+SELECT * FROM adventure_works_bronze.customer_address;
+
+-- COMMAND ----------
+
