27ea2eef207adeb7f35ae23e48353a6544828ef1
Luan Fonseca <34408591+tiluan@users.noreply.github.com>
2024-02-04 16:00:44 -0300

    Update: README.md dataset factory linkedService notebooks pipeline publish_config.json

---
+++ notebooks/hnk-Lakehouse/3 - Ouro/02 - v_product_and_description.sql
@@ -0,0 +1,22 @@
+-- Databricks notebook source
+CREATE OR REPLACE VIEW ADVENTURE_WORKS_PRATA.V_PRODUCT_AND_DESCRIPTION AS
+SELECT 
+  PD.ProductID,
+  PD.Name AS Name,
+  PDM.Name AS ProductModel,
+  PMPD.ProductModelID,
+  PMPD.Culture,
+  PDD.ProductDescriptionID,
+  PDD.Description
+FROM adventure_works_prata.product PD
+JOIN adventure_works_prata.product_model PDM ON PD.ProductModelID = PDM.ProductModelID
+JOIN adventure_works_prata.product_model_product_description PMPD ON PD.ProductModelID = PMPD.ProductModelID
+JOIN adventure_works_prata.product_description PDD ON PMPD.ProductDescriptionID = PDD.ProductDescriptionID
+ORDER BY 1
+
+-- COMMAND ----------
+
+SELECT * FROM ADVENTURE_WORKS_PRATA.V_PRODUCT_AND_DESCRIPTION;
+
+-- COMMAND ----------
+
