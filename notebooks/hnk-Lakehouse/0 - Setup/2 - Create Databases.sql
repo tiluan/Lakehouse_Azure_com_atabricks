f0502fd6f3838a6725a85d19410568f5fa3be0ed
Luan Fonseca <34408591+tiluan@users.noreply.github.com>
2024-02-03 21:09:59 -0300

    Update: README.md dataset factory linkedService notebooks pipeline publish_config.json

--- notebooks/hnk-Lakehouse/0 - Setup/2 - Create Databases.sql
+++ notebooks/hnk-Lakehouse/0 - Setup/2 - Create Databases.sql
@@ -1,3 +1,13 @@
 -- Databricks notebook source
 CREATE DATABASE bronze
 LOCATION '/mnt/stghnklcf/bronze/raw'
+
+-- COMMAND ----------
+
+CREATE DATABASE prata
+LOCATION '/mnt/stghnklcf/prata/raw'
+
+-- COMMAND ----------
+
+CREATE DATABASE ouro
+LOCATION '/mnt/stghnklcf/ouro/raw'
