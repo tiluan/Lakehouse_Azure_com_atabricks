7a28a95afafda640be7a59a4a6d8a90264790a02
Luan Fonseca <34408591+tiluan@users.noreply.github.com>
2024-02-05 21:01:48 -0300

    Update: README.md dataset factory linkedService notebooks pipeline publish_config.json

--- notebooks/hnk-Lakehouse/4 - Analise/01 - vendas_produtos.sql
+++ notebooks/hnk-Lakehouse/4 - Analise/01 - vendas_produtos.sql
@@ -1,15 +1,32 @@
 -- Databricks notebook source
-CREATE OR REPLACE TABLE adventure_works_ouro.report_vendas_categoria_ano_mes AS
+SELECT * FROM adventure_works_ouro.fato_vendas_produtos
+
+-- COMMAND ----------
 
 SELECT 
-  year(fato.OrderDate) AS Ano,
-  month(fato.OrderDate) as Mes,
-  cat.Name as Category_name,
-  SUM(fato.LineTotal) AS total_faturamento,
-  SUM(fato.OrderQty) AS quantidade_produtos,
-  (SUM(fato.LineTotal) / SUM(fato.OrderQty)) AS ticket_medio
-FROM adventure_works_ouro.fato_vendas_produtos fato
-JOIN adventure_works_prata.product product on fato.ProductID = fato.ProductID
-JOIN adventure_works_prata.product_category cat on product.ProductCategoryID = cat.ProductCategoryID
-GROUP BY year(fato.OrderDate), month(fato.OrderDate), cat.Name
-ORDER BY SUM(fato.LineTotal) DESC
+  ProductID,
+  SUM(LineTotal)
+FROM adventure_works_ouro.fato_vendas_produtos
+GROUP BY ProductID
+ORDER BY SUM(LineTotal) DESC
+
+-- COMMAND ----------
+
+SELECT 
+  ProductID,
+  SUM(LineTotal),
+  SUM(OrderQty)
+FROM adventure_works_ouro.fato_vendas_produtos
+GROUP BY ProductID
+ORDER BY SUM(LineTotal) DESC
+
+-- COMMAND ----------
+
+SELECT 
+  ProductID,
+  SUM(LineTotal),
+  SUM(OrderQty),
+  (SUM(LineTotal) / SUM(OrderQty)) AS ticket_medio
+FROM adventure_works_ouro.fato_vendas_produtos
+GROUP BY ProductID
+ORDER BY SUM(LineTotal) DESC
